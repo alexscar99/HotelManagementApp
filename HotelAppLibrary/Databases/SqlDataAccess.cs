4491d4a7b6e7376953b6ce87eb301ee02a6b0014
Alex Scarlett <alex.scarlett91@gmail.com>
2020-05-20 15:05:34 -0400

    Wire up Dapper as ORM, create SQL specific data access class using it
    Create 'SqlDataAccess' class with methods for loading data and saving data using Dapper. Both methods are written to work with SQL statements that are either text or stored procedures. Also, make interface for dependency injection use later on.

---
+++ HotelAppLibrary/Databases/SqlDataAccess.cs
@@ -0,0 +1,66 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace HotelAppLibrary.Databases
+{
+    public class SqlDataAccess : ISqlDataAccess
+    {
+        private readonly IConfiguration _config;
+
+        public SqlDataAccess(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public List<T> LoadData<T, U>(string sqlStatement,
+                                      U parameters,
+                                      string connectionStringName,
+                                      bool isStoredProcedure = false)
+        {
+            // Get connection string from config object, set default command type to be text
+            string connectionString = _config.GetConnectionString(connectionStringName);
+            CommandType commandType = CommandType.Text;
+
+            // Change command type to stored procedure if it is one
+            if (isStoredProcedure)
+            {
+                commandType = CommandType.StoredProcedure;
+            }
+
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                // 'Query' in Dapper returns a data set, type 'T' here will refer to the Model being used to get each row
+                List<T> rows = connection.Query<T>(sqlStatement, parameters, commandType: commandType).ToList();
+                return rows;
+            }
+        }
+
+        public void SaveData<T>(string sqlStatement,
+                                T parameters,
+                                string connectionStringName,
+                                bool isStoredProcedure = false)
+        {
+            // Get connection string from config object, set default command type to be text
+            string connectionString = _config.GetConnectionString(connectionStringName);
+            CommandType commandType = CommandType.Text;
+
+            // Change command type to stored procedure if it is one
+            if (isStoredProcedure)
+            {
+                commandType = CommandType.StoredProcedure;
+            }
+
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                // Execute SQL statement with parameters and command type
+                connection.Execute(sqlStatement, parameters, commandType: commandType);
+            }
+        }
+    }
+}
