555b7ca44907c8817c9ffe69a5d82a524cb05a89
Alex Scarlett <alex.scarlett91@gmail.com>
2020-05-20 17:40:19 -0400

    Create 'SqlData' class for working with SQL data. Write method to get all available room types.

---
+++ HotelAppLibrary/Data/SqlData.cs
@@ -0,0 +1,30 @@
+﻿using HotelAppLibrary.Databases;
+using HotelAppLibrary.Models;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HotelAppLibrary.Data
+{
+    public class SqlData
+    {
+        private readonly ISqlDataAccess _db;
+        private const string connectionStringName = "SqlDb";
+
+        public SqlData(ISqlDataAccess db)
+        {
+            _db = db;
+        }
+
+        public List<RoomTypeModel> GetAvailableRoomTypes(DateTime startDate, DateTime endDate)
+        {
+            // Load and return data using stored procedure
+            return _db.LoadData<RoomTypeModel, dynamic>("dbo.spRoomTypes_GetAvailableTypes",
+                                                          new { startDate, endDate },
+                                                          connectionStringName,
+                                                          true);
+        }
+    }
+}
