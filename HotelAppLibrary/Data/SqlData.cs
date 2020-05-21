8338e837c2f9394ecc26f6aa3b13240fd9709271
Alex Scarlett <alex.scarlett91@gmail.com>
2020-05-21 17:35:19 -0400

    Write 'BookGuest' method to book a guest
    Create 3 stored procedures. One inserts a guest into 'Guests' table, another gets available rooms from 'Rooms' table, and the 3rd creates and inserts a booking into 'Bookings' table.

--- HotelAppLibrary/Data/SqlData.cs
+++ HotelAppLibrary/Data/SqlData.cs
@@ -26,5 +26,40 @@ namespace HotelAppLibrary.Data
                                                           connectionStringName,
                                                           true);
         }
+
+        public void BookGuest(string firstName, string lastName, DateTime startDate, DateTime endDate, int roomTypeId)
+        {
+            // Insert to 'Guests' table and then retrieve that guest to eventually book them
+            GuestModel guest = _db.LoadData<GuestModel, dynamic>("dbo.spGuests_Insert",
+                                                                 new { firstName, lastName },
+                                                                 connectionStringName,
+                                                                 true).First();
+
+            // To calculate total cost for the booking, need the room type and how many days were stayed
+            RoomTypeModel roomType = _db.LoadData<RoomTypeModel, dynamic>("select * from dbo.RoomTypes where Id = @Id",
+                                                                          new { Id = roomTypeId },
+                                                                          connectionStringName,
+                                                                          false).First();
+            TimeSpan timeStaying = endDate.Date.Subtract(startDate.Date);
+
+            // Get available rooms that fit the criteria of the dates desired and the room type
+            List<RoomModel> availableRooms = _db.LoadData<RoomModel, dynamic>("dbo.spRooms_GetAvailableRooms",
+                                                                              new { startDate, endDate, roomTypeId },
+                                                                              connectionStringName,
+                                                                              true);
+
+            // Save booking by inserting into 'Bookings' table
+            _db.SaveData("dbo.spBookings_Insert",
+                         new 
+                         { 
+                             roomId = availableRooms.First().Id, 
+                             guestId = guest.Id,
+                             startDate,
+                             endDate,
+                             totalCost = timeStaying.Days * roomType.Price
+                         },
+                         connectionStringName,
+                         true);
+        }
     }
 }
